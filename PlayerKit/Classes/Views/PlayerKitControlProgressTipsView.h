63915775fe2ffbad3e455c1ff04f07d0777fc59a
Jack <xhzengAIB@gmail.com>
2016-03-15 13:55:41 +0800

    修改描述

--- PlayerKit/Classes/Views/PlayerKitControlProgressTipsView.h
+++ PlayerKit/Classes/Views/PlayerKitControlProgressTipsView.h
@@ -3,8 +3,7 @@
 //  PlayerKit
 //
 //  Created by Jack MacBook on 15/12/31.
-//  Copyright © 2015年 All rights reserved.
-//  嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，现任广州华秀软件技术有限公司的CEO，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://www.zengxianhua.com
+//  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 
--- PlayerKitDemo/PlayerKitDemo/Demos/Youku/YoukuDetailViewController.m
+++ PlayerKitDemo/PlayerKitDemo/Demos/Youku/YoukuDetailViewController.m
@@ -3,10 +3,8 @@
 //  PlayerKitDemo
 //
 //  Created by Jack_iMac on 16/1/18.
-//  Copyright © 2016年 All rights reserved.
-//  嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，现任广州华秀软件技术有限公司的CEO，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 543413507@QQ.COM:543413507 主页:http://www.zengxianhua.com
+//  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
-
 #import "YoukuDetailViewController.h"
 #import <PlayerKitContainer.h>
 
